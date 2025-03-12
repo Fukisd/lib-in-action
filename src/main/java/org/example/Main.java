1e35be036ffa0fc4287838836220603f6f93f6fb
Xuan Nhi <letranxuannhi9a3@gmail.com>
2025-03-12 08:35:41 +0700

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import com.morbious.mathutil.core.MathUtility;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtility.getFactorial(10));
+    }
+}
